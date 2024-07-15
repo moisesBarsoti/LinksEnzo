34b2893fa9f0262e6aa7d045a5af241ad21c3fa8
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-07-14 22:42:35 -0300

    Subindo Arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
 // Selecionando Arquivos
 const tituloDosLinksShop = document.querySelector('#tituloLinks h2#shop');
 const tituloDosLinks2 = document.querySelector('#tituloLinks h2#links2');
+const containerLinks = document.querySelector('#containerLinks');
+
+tituloDosLinksShop.addEventListener('click', () => {
+    if(tituloDosLinks2.classList.contains('ativo')) {
+        tituloDosLinks2.classList.remove('ativo');
+        tituloDosLinksShop.classList.add('ativo');
+    } 
+    containerLinks.style.opacity = '0';
+    tituloDosLinksShop.style.marginRight = '-1rem';
+});
+
+tituloDosLinks2.addEventListener('click', () => {
+    if(tituloDosLinksShop.classList.contains('ativo')) {
+        tituloDosLinksShop.classList.remove('ativo');
+        tituloDosLinks2.classList.add('ativo');
+    } 
+    containerLinks.style.opacity = '1';
+})
