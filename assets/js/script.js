87798e7ae21b54b9e3202084cd1d3438c3895f05
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-07-17 23:07:45 -0300

    Subindo Arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 // Selecionando Arquivos
 const inputCheckbox = document.querySelector("div#switchLinks input");
 const containerLinks = document.querySelector("div#containerLinks");
+const shop = document.querySelector("div#shop");
+
+shop.style.opacity = '0';
 
 inputCheckbox.addEventListener('click', () => {
     containerLinks.classList.toggle('desativo');
+    shop.style.opacity = '1';
+    shop.style.position = 'absolute';
 });
