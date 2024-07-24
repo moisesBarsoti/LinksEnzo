f8cd1415ee136f3c1f3cb52e92259af1579830e1
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-07-24 20:10:44 -0300

    Subindo Arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
 // Selecionando Arquivos
 const inputCheckbox = document.querySelector("div#switchLinks input");
-const containerLinks = document.querySelector("div#containerLinks");
+const containerLinks = document.querySelector("div#containerLinks div#links");
 const shop = document.querySelector("div#shop");
 
-shop.style.opacity = '0';
+shop.style.transform = 'translateX(-100vw)';
 
 inputCheckbox.addEventListener('click', () => {
     containerLinks.classList.toggle('desativo');
-    shop.style.opacity = '1';
-    shop.style.position = 'absolute';
+    if(containerLinks.classList.contains('desativo')) {
+        shop.style.transform = 'translateX(0vw)';
+        containerLinks.style.transform = 'translateX(-100vw)';
+        shop.style.position = 'absolute';
+
+    } else {
+        shop.style.transform = 'translateX(-100vw)';
+        containerLinks.style.transform = 'translateX(0vw)';
+        containerLinks.style.marginLeft = '-.5rem';
+    }
 });
