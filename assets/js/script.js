cdf283cb2a460e21263cb30dc56778446c216645
Moisés Barsoti <146322015+moisesBarsoti@users.noreply.github.com>
2024-07-26 17:02:16 -0300

    Subindo Arquivos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,9 @@ const switchInput = document.querySelector('div#switchLinks input');
 const links = document.querySelector('div#links');
 const shop = document.querySelector('div#shop');
 
+shop.classList.add('opacity0');
+
 switchInput.addEventListener('click', () => {
     links.classList.toggle('irParaBaixo');
-    shop.style.opacity = '1';
+    shop.classList.toggle('opacity0');
 });
